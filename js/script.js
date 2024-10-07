cc2c89a60660eaef076833cf87db56f7dcaeae57
lollobyte06 <84871459+lollobyte06@users.noreply.github.com>
2024-10-07 19:31:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.getElementById("chisono-btn").addEventListener("click", function () {
+    window.location.href = "../Website/about.html#chat"
+});
+
+document.getElementById("competenze-btn").addEventListener("click", function () {
+    window.location.href = "../Website/competenze.html#chat"
+});
+
+document.getElementById("esperienze-btn").addEventListener("click", function () {
+    window.location.href = "../Website/lavori.html#chat"
+});
+
+
+document.getElementById("progetti-btn").addEventListener("click", function () {
+    window.location.href = "../Website/progetti.html#chat"
+});
+
+document.getElementById("contatti-btn").addEventListener("click", function () {
+    window.location.href = "../Website/contatti.html#chat"
+});
+
+
+
+
+
