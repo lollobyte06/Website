1ae95f67666b948a226bff6dc12af85f62741746
lollobyte06 <84871459+lollobyte06@users.noreply.github.com>
2024-10-08 17:56:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 document.getElementById("chisono-btn").addEventListener("click", function () {
-    window.location.href = "../Website/about.html#chat"
+    window.location.href = "../about.html#chat"
 });
 
 document.getElementById("competenze-btn").addEventListener("click", function () {
-    window.location.href = "../Website/competenze.html#chat"
+    window.location.href = "../competenze.html#chat"
 });
 
 document.getElementById("esperienze-btn").addEventListener("click", function () {
-    window.location.href = "../Website/lavori.html#chat"
+    window.location.href = "../lavori.html#chat"
 });
 
 
 document.getElementById("progetti-btn").addEventListener("click", function () {
-    window.location.href = "../Website/progetti.html#chat"
+    window.location.href = "../progetti.html#chat"
 });
 
 document.getElementById("contatti-btn").addEventListener("click", function () {
-    window.location.href = "../Website/contatti.html#chat"
+    window.location.href = "../contatti.html#chat"
 });
 
 
